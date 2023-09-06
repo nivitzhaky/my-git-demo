3e43e15a435d48c16e1bd6038499865a63403406
nivitzhaky <niv.itzhaky@gmail.com>
2023-09-06 11:26:38 +0300

    commit from b1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world from branch 2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
