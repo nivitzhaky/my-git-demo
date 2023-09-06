2a1221a683247e75e9c6da09b17912228be9258c
nivitzhaky <niv.itzhaky@gmail.com>
2023-09-06 11:47:27 +0300

    bad commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello world from branch 2 considering branch 1");
+		System.out.println("bad commit");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
